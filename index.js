b33b8516fe31c1a05dac69489619e4582ef29919
Guang <jouyang19@proton.me>
2024-04-29 10:29:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+}
+
+function wrapAdjective(emphasis = "*") {
+    return function youAre (adjective) {
+        let IdStatement = `You are ${emphasis}${adjective}${emphasis}!`;
+        return IdStatement;
+    };
+}
